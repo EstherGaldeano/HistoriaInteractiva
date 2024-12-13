2735d580461041a2c2c4d62ef264ef7e8c912c81
Retse_dev <stergaldeano@gmail.com>
2024-12-13 16:57:50 +0100

    arreglos

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -18,6 +18,9 @@ public class DialogueTrigger : MonoBehaviour
         if(is_in_talk_area && Input.GetKeyDown(KeyCode.E)){
             Debug.Log("Entra");
             dialogueRunner.StartDialogue("Nyx");
+            //pte código que muestre las dos raw images con los personajes que hablan
+            //Player siempre el mismo, NPC según TAG
+            //Bloquear movimiento de jugador durante el díalogo
         }
         
     
