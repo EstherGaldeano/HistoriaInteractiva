848f3ebdb8eb01ac100486992a76c5ff0fe20538
Retse_dev <stergaldeano@gmail.com>
2024-12-15 19:24:47 +0100

    all animations working

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,10 +7,12 @@ public class PlayerMovement : MonoBehaviour
     
     private Rigidbody2D rigidBody;
     private Vector2 moveInput;
+    private Animator animator;
 
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
 
@@ -20,6 +22,17 @@ public class PlayerMovement : MonoBehaviour
     }
 
     public void Move(InputAction.CallbackContext context){
+        animator.SetBool("IsWalking", true);
+        
+        if(context.canceled){
+            animator.SetBool("IsWalking",false);
+            animator.SetFloat("LastInputX", moveInput.x);
+            animator.SetFloat("LastInputY", moveInput.y);
+        }
+
+        
         moveInput = context.ReadValue<Vector2>();
+        animator.SetFloat("InputX", moveInput.x);
+        animator.SetFloat("InputY", moveInput.y);
     }
 }
